3a8279bf1bfa54491b916aba2b8c69835e5d8c46
miidlaj <mumidlaj@gmail.com>
2023-08-03 22:28:25 +0530

    eureka

--- src/main/java/com/midlaj/bookingservice/BookingServiceApplication.java
+++ src/main/java/com/midlaj/bookingservice/BookingServiceApplication.java
@@ -2,15 +2,14 @@ package com.midlaj.bookingservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-@EnableEurekaClient
 public class BookingServiceApplication {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(BookingServiceApplication.class, args);
 	}
 
